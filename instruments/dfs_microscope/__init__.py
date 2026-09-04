3e5e37dd51a084a98cb4a18e5bf352488f46d8ce
TimKo90 <tim.kodalle@gmail.com>
2026-09-03 19:25:43 -0700

    Add dfs_microscope instrument

---
+++ instruments/dfs_microscope/__init__.py
@@ -0,0 +1,9 @@
+NAME = 'dfs_microscope'
+INGESTOR = 'SingleSpecScopeFoundryH5Ingestor'
+UI_MODE = 'standard'
+HOLDER_LAYOUTS = {}
+DEFAULT_HOLDER_LAYOUT = ''
+FLOW = None
+POST_PROCESSING = []
+PANEL_TEMPLATE = None
+FILE_PARSER = None
